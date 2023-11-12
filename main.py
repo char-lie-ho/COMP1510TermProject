93178a1b4dc47409ea4ba24e2c4d622bd1cc0ee4
ho921 <tho124@my.bcit.ca>
2023-11-11 22:25:57 -0800

    Add player dictionary

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def create_character():
     character_name = input("Tell me, what is your name? ")
     print("So, your name is %s" % character_name)
     proceed = input("Is this correct? (Y/N) ").upper()
+    # check if users want to change their mind
     while proceed != 'Y':
         proceed = input("Is this correct? (Y/N) ").upper()
+    player = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5, "Level": 1, "Name": character_name}
     print("OK. Let's begin!")
+    return player
 
 
 def game():
