f54ff412402fc8722e176a607fdfbc0aa0b95275
ho921 <tho124@my.bcit.ca>
2023-11-11 23:13:23 -0800

    Add game_difficulty

--- main.py
+++ main.py
@@ -12,12 +12,31 @@ def create_character():
     while proceed != 'Y':
         proceed = input("Is this correct? (Y/N) ").upper()
     player = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5, "Level": 1, "Name": character_name}
-    print("OK. Let's begin!")
     return player
+
+
+def game_difficulty():
+    # print(int(difficulty) not in [1, 2, 3])
+
+    while True:
+        difficulty = input("Life is hard, how hard you want this adventure to be on a scale "
+                           "from 1 (easiest) to 3 (difficult)? ")
+        try:
+            float(difficulty)
+        except ValueError:
+            print("Oh, you can't even type numbers?!")  # if user input is not a number
+        else:
+            if float(difficulty) not in [1, 2, 3]:  # if user input is not an integer
+                print("Please enter an integer.")
+                continue
+            else:
+                print("Great! I like your choice.")
+                return difficulty
 
 
 def game():
     create_character()
+    game_difficulty()
 
 
 def main():
