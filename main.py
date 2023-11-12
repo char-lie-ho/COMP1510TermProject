42aaee3ba539388de2d28ef98b6b2f81226e6c69
ho921 <tho124@my.bcit.ca>
2023-11-11 22:18:56 -0800

    Add create character function

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ A01358146
 """
 
 
+def create_character():
+    character_name = input("Tell me, what is your name? ")
+    print("So, your name is %s" % character_name)
+    proceed = input("Is this correct? (Y/N) ").upper()
+    while proceed != 'Y':
+        proceed = input("Is this correct? (Y/N) ").upper()
+    print("OK. Let's begin!")
+
+
 def game():
-    pass
+    create_character()
 
 
 def main():
     """
     Execute the game function.
     """
+    print('-' * 80)
+    print("\t \t Welcome!")
+    print('-' * 80)
     game()
 
 
